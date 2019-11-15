6d38742e81737e639fa55a67513465b102e9ca92
Ludwig Richter <richter@fliegwerk.com>
2019-11-15 12:29:23 +0100

    Added shebang

--- bin/cbm-cli.js
+++ bin/cbm-cli.js
@@ -1,12 +1,11 @@
-console.log(process.argv);
+#!/usr/bin/env node
 
+const script = process.argv.slice(1);
 
-// const script = process.argv.slice(1);
-
-// if (['build', 'start', 'test', 'test-cli'].includes(script)) {
-//     const returnValue = require.resolve('../scripts/' + script)() || 0;
-//     process.exit(returnValue);
-// } else {
-//     console.log('Unknown script: ' + script);
-//     console.log('Valid options are: \'build\', \'start\', \'test\', \'test-cli\'');
-// }
+if (['build', 'start', 'test', 'test-cli'].includes(script)) {
+    const returnValue = require.resolve('../scripts/' + script)() || 0;
+    process.exit(returnValue);
+} else {
+    console.log('Unknown script: ' + script);
+    console.log('Valid options are: \'build\', \'start\', \'test\', \'test-cli\'');
+}
