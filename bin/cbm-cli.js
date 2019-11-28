b465caac9b47357ca9ab89e0f93093982d395245
Ludwig <richter@fliegwerk.com>
2019-11-28 22:12:51 +0100

    Changed script resolving method

--- bin/cbm-cli.js
+++ bin/cbm-cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const script = process.argv.slice(1);
+const script = process.argv.slice(2)[0];
 
 if (['build', 'start', 'test', 'test-cli'].includes(script)) {
-    const returnValue = require.resolve('../scripts/' + script)() || 0;
+    const returnValue = require('../scripts/' + script)() || 0;
     process.exit(returnValue);
 } else {
     console.log('Unknown script: ' + script);
--- scripts/start.js
+++ scripts/start.js
@@ -38,4 +38,4 @@ module.exports = function() {
 
         console.log('\x1b[1;33m%s\x1b[0m', 'Starting CBM test environment ...');
     });
-}
+};
